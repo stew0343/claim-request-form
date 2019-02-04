07229c46305805d8417d70fe16380ae2be6c3c27
Michelle Stewart <stew0343@algonquinlive.com>
2019-02-04 16:38:41 -0500

    updates to input fields

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -2,13 +2,51 @@ export default {
     name: 'claimForm',
     data() {
         return {
-            firstName: '',
-            lastName: '',
-            email: '',
-            subject: '',
-            comments: '',
-            spam: '',
-            dialogVisible: false
-        };
-    }
+            form: {
+                firstName: '',
+                lastName: '',
+                email: '',
+                subject: '',
+                comments: '',
+                spam: '',
+                dialogVisible: false
+            },
+            rules: {
+                firstName: [
+                  { required: true, message: 'Please input first name', trigger: 'blur' }
+                ],
+                lastName: [
+                  { required: true, message: 'Please input last name', trigger: 'change' }
+                ],
+                email: [
+                  { required: true, message: 'Please input email', trigger: 'change' }
+                ],
+                type: [
+                  { required: true, message: 'Please enter the type of claim', trigger: 'change' }
+                ],
+                details: [
+                  { required: true, message: 'Please enter details of your claim', trigger: 'change' }
+                ],
+                receipt: [
+                  { required: false, message: 'You must request a receipt', trigger: 'blur' }
+                ]
+            }
+        }
+    },
+    methods: {
+        submitForm(form) {
+          this.$refs[form].validate((valid) => {
+            if (valid) {
+                this.form.dialogVisible = true;
+            } else {
+                // eslint-disable-next-line
+              console.log('error submit!!');
+              return false;
+            }
+          });
+        },
+        resetForm(form) {
+          this.$refs[form].resetFields();
+        }
+      }
 }
--- src/components/form/form.vue
+++ src/components/form/form.vue
@@ -1,61 +1,60 @@
 <template>
   <div class="hello">
-    <form>
-      <el-input placeholder="First Name" v-model="firstName"></el-input>
-      <el-input placeholder="Last Name" v-model="lastName"></el-input>
-      <el-input placeholder="Email" v-model="email"></el-input>
-      <el-input placeholder="Subject" v-model="subject"></el-input>
-      <el-input
-        type="textarea"
-        :rows="2"
-        placeholder="Comments"
-        v-model="comments">
-      </el-input>
-      <el-checkbox v-model="spam">Subscribe me to spam</el-checkbox>
+    <el-form :model="form" :rules="rules" ref="form" label-width="120px">
+      <el-form-item label="First Name" prop="firstName" required>
+        <el-input placeholder="First Name" v-model="form.firstName"></el-input>
+      </el-form-item>
+
+      <el-form-item label="Last Name" prop="lastName">
+        <el-input placeholder="Last Name" v-model="form.lastName"></el-input>
+      </el-form-item>
+
+      <el-form-item label="Email" prop="email">
+        <el-input placeholder="Email" v-model="form.email"></el-input>
+      </el-form-item>
+
+      <el-form-item label="Type of Claim" prop="type">
+        <el-input placeholder="Type of Claim" v-model="form.type"></el-input>
+      </el-form-item>
+
+      <el-form-item label="Claim Details" prop="details">
+        <el-input
+          type="textarea"
+          :rows="2"
+          placeholder="Claim Details"
+          v-model="form.details">
+        </el-input>
+      </el-form-item>
+
+      <el-form-item label="Receipt" prop="receipt">
+        <el-checkbox v-model="form.receipt">I'd like an e-mail receipt</el-checkbox>
+      </el-form-item>
+
+      <el-form-item>
       <el-row>
-        <el-button type="success" @click="dialogVisible = true">Submit</el-button>
-        <el-button type="danger">Cancel</el-button>
+        <el-button type="success" @click="submitForm('form')">Submit</el-button>
+        <el-button type="danger" @click="resetForm('form')">Cancel</el-button>
       </el-row>
+      </el-form-item>
 
-
-      <el-dialog
-        title="Confirm"
-        :visible.sync="dialogVisible"
-        width="30%"
-        :before-close="handleClose">
-        <span>Are you sure???</span>
-        <span slot="footer" class="dialog-footer">
-          <el-button @click="dialogVisible = false">Cancel</el-button>
-          <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
-        </span>
-      </el-dialog>
-    </form>
+      <el-form-item>
+        <el-dialog
+          title="Confirm"
+          :visible.sync="form.dialogVisible"
+          width="30%"
+          :before-close="handleClose">
+          <span>Are you sure???</span>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="form.dialogVisible = false">Cancel</el-button>
+            <el-button type="primary" @click="form.dialogVisible = false">Confirm</el-button>
+          </span>
+        </el-dialog>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
-<script src="./form.js">
-// export default {
-//   name: 'HelloWorld',
-//   props: {
-//     msg: String
-//   }
-// }
-</script>
+<script src="./form.js"></script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped src="./form.css">
-/* h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-} */
-</style>
+<style scoped src="./form.css"></style>
